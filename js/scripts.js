21e09ee9d72701834b9c3c8d043c29e7990eb21f
alex & jordan <student@epicodus.com>
2015-06-02 14:06:24 -0700

    add working front and back end

--- js/scripts.js
+++ js/scripts.js
@@ -9,3 +9,52 @@ BankAccount.prototype.deposit = function(number) {
 BankAccount.prototype.withdrawal = function(number) {
   this.balance = this.balance - number;
 }
+
+function resetFields() {
+  $("input#new-name").val("");
+  $("input#initial-deposit").val("");
+  $("input#deposit").val("");
+  $("input#withdrawal").val("");
+}
+
+
+$(document).ready(function(){
+  event.preventDefault();
+
+  var currentAccount1 = 0;
+
+  $("form#new-account").submit(function(event) {
+    event.preventDefault();
+
+    var inputName = $("input#new-name").val();
+    var inputStartDeposit = parseInt($("input#initial-deposit").val());
+    var newAccount = new BankAccount(inputName, inputStartDeposit);
+    $(".current-balance").text(newAccount.balance);
+
+    resetFields();
+    currentAccount1 = newAccount;
+  });
+
+  $("form#deposit-withdrawal").submit(function(event) {
+    event.preventDefault();
+
+    var newAccount = currentAccount1;
+    var inputDeposit = parseInt($("input#deposit").val());
+    var inputWithdrawal = parseInt($("input#withdrawal").val());
+
+    if (isNaN(inputDeposit)===false) {
+      newAccount.deposit(inputDeposit);
+    } else {
+      newAccount.withdrawal(inputWithdrawal);
+    };
+
+    $(".current-balance").text(newAccount.balance);
+
+    resetFields();
+
+    currentAccount1 = newAccount;
+
+
+  });
+
+});
