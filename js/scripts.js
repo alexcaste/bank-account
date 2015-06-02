6c6097385663974667912189434713757f8161c1
alex & jordan <student@epicodus.com>
2015-06-02 09:50:49 -0700

    add deposit and withdrawl methods passing specs

--- js/scripts.js
+++ js/scripts.js
@@ -12,3 +12,10 @@ function BankAccount(acctName, balance) {
   this.acctName = acctName;
   this.balance = balance;
 }
+BankAccount.prototype.deposit = function(number) {
+  this.balance = this.balance + number;
+}
+
+BankAccount.prototype.withdrawal = function(number) {
+  this.balance = this.balance - number;
+}
--- spec/specs.js
+++ spec/specs.js
@@ -4,4 +4,17 @@ describe('BankAccount', function(){
     expect(testAccount.acctName).to.equal("Savage");
     expect(testAccount.balance).to.equal(100000000000);
   });
+
+  it("add a given amount to the balance of an acct", function(){
+    var testAccount = new BankAccount("Savage", 100);
+    testAccount.deposit(100);
+    expect(testAccount.balance).to.equal(200);
+  });
+
+  it("subtract a given amount to the balance of an acct", function(){
+    var testAccount = new BankAccount("Savage", 200);
+    testAccount.withdrawal(100);
+    expect(testAccount.balance).to.equal(100);
+  });
+
 });
